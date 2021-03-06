05f380f9923602632a94e8ed31f1b5af37c3cab5
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-06 18:23:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
     DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
-    binding.prt
+    binding.pry
 
     table_info = DB[:conn].execute(sql)
     column_names = []
